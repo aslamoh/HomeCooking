4c8cf74f72f0866c195368f05312d73474812069
aslamoh <smaslam2018@gmail.com>
2020-10-16 21:38:45 -0600

    create ejs view folder and create menu.ejs

--- app.js
+++ app.js
@@ -1,32 +1,19 @@
+const path = require('path');
 const express = require('express');
+//const dotenv = require('dotenv').config();
 
 const app = express();
+app.set('view engine', 'ejs');
 
+app.use(express.static(path.join(__dirname, 'public')));
 
-// // Views in public directory
- app.use(express.static('public'));
+app.get('/', function(request, response) {
+	response.render('index', {});
+});
 
-// Main, error and success views
- app.get('/', function (request, response) {
-  response.sendFile(__dirname + '/public/index');
- });
-
-// app.get('/', function (request, response) {
-//   response.send('welcome to.....');
-// })
-
-
-
-// app.get("/success", function (request, response) {
-//   response.sendFile(__dirname + '/public/success.html');
-// });
-
-// app.get("/error", function (request, response) {
-//   response.sendFile(__dirname + '/public/error.html');
-// });
-
-
-
+app.get('/menu', function(request, response) {
+	response.render('menu', {});
+});
 
 
 /// start up server
